5f243c6a75fb999b5a2cbd786505cbbcf5666ca2
Tanakaryuki <tnk3257.rt@gmail.com>
2023-12-22 03:56:40 +0900

    fix: 引数を修正

--- api/routers/user.py
+++ api/routers/user.py
@@ -15,7 +15,7 @@ router = APIRouter()
 
 @router.post("/signup", description="新しいアカウントを作成するために使用されます。", tags=["users"])
 def signup(request: user_schema.UserSignupRequest, db: Session = Depends(get_db)):
-    user = user_crud.read_user_by_username(db, id=request.id)
+    user = user_crud.read_user_by_username(db, username=request.username)
     if user:
         raise HTTPException(status.HTTP_400_BAD_REQUEST)
     user = user_crud.creatr_user(db, request)
