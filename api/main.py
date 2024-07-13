d16ca301912d51f632eb4ae097f3dae67c0c6d58
Tanakaryuki <tnk3257.rt@gmail.com>
2024-07-14 00:12:04 +0900

    add: health check

--- api/main.py
+++ api/main.py
@@ -3,5 +3,9 @@ from fastapi import FastAPI
 from api.routers import user,task
 
 app = FastAPI()
+@app.get("/")
+async def hello():
+    return {"message": "Hello World"}
+
 app.include_router(user.router, prefix="/api", tags=["users"])
 app.include_router(task.router, prefix="/api", tags=["tasks"])
