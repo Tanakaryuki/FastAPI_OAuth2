8e9f4c3e1d7e2096532bfbf6d702e7d16c57c8db
Tanakaryuki <tnk3257.rt@gmail.com>
2024-08-05 15:06:43 +0900

    update: SQLAlchemy imports for sessionmaker and declarative_base

--- api/db.py
+++ api/db.py
@@ -1,7 +1,6 @@
 from uuid import uuid4
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker,declarative_base
 import os
 from dotenv import load_dotenv
 
--- api/reset_db.py
+++ api/reset_db.py
@@ -1,18 +1,20 @@
 from sqlalchemy import create_engine
-from api.models.user import Base
-from api.models.task import Base
+from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
-DATABASE_URL = str(os.getenv("DATABASE_URL"))
+from api.models.user import Base as UserBase
+from api.models.task import Base as TaskBase
 
+load_dotenv(verbose=True)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 def reset_database(db_url):
     engine = create_engine(db_url, echo=True)
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-
+    UserBase.metadata.drop_all(bind=engine)
+    TaskBase.metadata.drop_all(bind=engine)
+    UserBase.metadata.create_all(bind=engine)
+    TaskBase.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     reset_database(DATABASE_URL)
